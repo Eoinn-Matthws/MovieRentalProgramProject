656145304c839c531271106b6ec4a779995aeebf
Eoinn-Matthews <owen.matthews01@student.wandw.ac.nz>
2026-05-23 12:08:14 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieRentalProgramProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
